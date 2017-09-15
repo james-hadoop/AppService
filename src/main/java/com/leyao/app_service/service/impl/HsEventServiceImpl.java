487d403ada212ca7fd5113a0c120b34b378bf9e5
Jiang Qian <james_hadoop@163.com>
2017-09-15 16:47:15 +0800

    update

--- src/main/java/com/leyao/app_service/service/impl/HsEventServiceImpl.java
+++ src/main/java/com/leyao/app_service/service/impl/HsEventServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.leyao.app_service.common.GlobalConstant;
 import com.leyao.app_service.common.Response;
@@ -195,6 +196,7 @@ public class HsEventServiceImpl implements IHsEventService {
     }
 
     @Override
+    @Transactional
     public int postTEventSummary(TEventSummary tEventSummary) {
         Long hEventId = tEventSummary.gethEventId();
         if (null == hEventId) {
