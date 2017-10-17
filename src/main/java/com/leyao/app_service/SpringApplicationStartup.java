05bc26bbc031a800302a321f1187e43800c39062
Jiang Qian <james_hadoop@163.com>
2017-10-17 18:49:23 +0800

    update

--- src/main/java/com/leyao/app_service/SpringApplicationStartup.java
+++ src/main/java/com/leyao/app_service/SpringApplicationStartup.java
@@ -3,7 +3,6 @@ package com.leyao.app_service;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
-import com.leyao.app_service.dao.configuration.DimensionConfig;
 import com.leyao.app_service.service.IDimensionConfigService;
 
 public class SpringApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
@@ -13,7 +12,5 @@ public class SpringApplicationStartup implements ApplicationListener<ContextRefr
         IDimensionConfigService dimensionConfigService = event.getApplicationContext()
                 .getBean(IDimensionConfigService.class);
         dimensionConfigService.initDimensionConfig();
-        System.out.println(
-                "DimensionConfig.rEventCategoryEnumList.size()=" + DimensionConfig.rEventCategoryEnumList.size());
     }
 }
